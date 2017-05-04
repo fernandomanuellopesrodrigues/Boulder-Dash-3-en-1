3a460f73d5ab2d3e97dd08af4dc97cd14b8d5d62
Celso Rodrigues <basto.salt@gmail.com>
2017-05-04 11:48:44 +0200

    changes mineurs

--- src/entitees/tickables/Diamant.java
+++ src/entitees/tickables/Diamant.java
@@ -9,44 +9,52 @@ import static entitees.abstraites.Entitee.Entitees.Rockford;
 
 import entitees.abstraites.Entitee;
 import entitees.abstraites.Tickable;
+import main.Partie;
 
 public class Diamant extends Tickable {
 
-	public Diamant(int x, int y) {
-		super(x, y);
-		setDestructible(true);
-		getDeplacementsPossibles().add(Rockford);
-		getDeplacementsPossibles().add(MurMagique);
-		getDeplacementsPossibles().add(Libellule);
-		getDeplacementsPossibles().add(Luciole);
-		getDeplacementsPossibles().add(Explosion);
-		enumeration = Diamant;
-	}
+    public Diamant(int x, int y) {
+        super(x, y);
+        setDestructible(true);
+        getDeplacementsPossibles().add(Rockford);
+        getDeplacementsPossibles().add(MurMagique);
+        getDeplacementsPossibles().add(Libellule);
+        getDeplacementsPossibles().add(Luciole);
+        getDeplacementsPossibles().add(Explosion);
+        enumeration = Diamant;
+    }
 
-	@Override
-	protected int contactAutreEntitee(Entitee entitee) {
-		setDirection('b');
-		if (entitee.is(Rockford)) {
-			mourir();
-			((Rockford) entitee).ramasserDiamant();
-			return -1;
-		} else if (entitee.is(MurMagique)) {
-			return 0;
-		} else if (entitee.is(Libellule)) {
-			exploser(false);
-			return 0;
-		} else if (entitee.is(Luciole)) {
-			exploser(true);
-			return 0;
-		} else if (entitee.is(Explosion)) {
-			mourir();
-			return -1;
-		}
-		return 1;
-	}
+    @Override
+    protected int contactAutreEntitee(Entitee entitee) {
+        setDirection('b');
+        if (entitee.is(Rockford)) {
+            mourir();
+            ((Rockford) entitee).ramasserDiamant();
+            return -1;
+        } else if (entitee.is(MurMagique)) {
+            return 0;
+        } else if (entitee.is(Libellule)) {
+            exploser(false);
+            return 0;
+        } else if (entitee.is(Luciole)) {
+            exploser(true);
+            return 0;
+        } else if (entitee.is(Explosion)) {
+            mourir();
+            return -1;
+        }
+        return 1;
+    }
 
-	@Override
-	public void tick() {
-		gererChute();
-	}
+    @Override
+    public void tick() {
+        rockfordEndessous();
+        gererChute();
+    }
+
+    private void rockfordEndessous() {
+        if (Partie.gererNiveau.getNiveau().getMap()[getX()][getY() + 1].is(Rockford)) {
+            setChute(true);
+        }
+    }
 }
--- src/entitees/tickables/Rockford.java
+++ src/entitees/tickables/Rockford.java
@@ -1,104 +1,103 @@
 package entitees.tickables;
 
-import static entitees.abstraites.Entitee.Entitees.Amibe;
-import static entitees.abstraites.Entitee.Entitees.Diamant;
-import static entitees.abstraites.Entitee.Entitees.Explosion;
-import static entitees.abstraites.Entitee.Entitees.Pierre;
-import static entitees.abstraites.Entitee.Entitees.Poussiere;
-import static entitees.abstraites.Entitee.Entitees.Rockford;
-import static entitees.abstraites.Entitee.Entitees.Sortie;
-import static entitees.abstraites.Entitee.Entitees.Vide;
-
 import entitees.abstraites.Entitee;
 import entitees.abstraites.Tickable;
 import main.Partie;
 
+import static entitees.abstraites.Entitee.Entitees.*;
+
 public class Rockford extends Tickable {
 
-	private char ancienneDirection = 'd';
+    private char ancienneDirection = 'd';
 
-	public Rockford(int x, int y) {
-		super(x, y);
-		setDestructible(true);
-		setDirection(' ');
-		getDeplacementsPossibles().add(Poussiere);
-		getDeplacementsPossibles().add(Diamant);
-		getDeplacementsPossibles().add(Pierre);
-		getDeplacementsPossibles().add(Sortie);
-		getDeplacementsPossibles().add(Amibe);
-		getDeplacementsPossibles().add(Explosion);
-		enumeration = Rockford;
-	}
+    public Rockford(int x, int y) {
+        super(x, y);
+        setDestructible(true);
+        setDirection(' ');
+        getDeplacementsPossibles().add(Poussiere);
+        getDeplacementsPossibles().add(Diamant);
+        getDeplacementsPossibles().add(Pierre);
+        getDeplacementsPossibles().add(Sortie);
+        getDeplacementsPossibles().add(Amibe);
+        getDeplacementsPossibles().add(Explosion);
+        enumeration = Rockford;
+    }
 
-	public void ramasserDiamant() {
-		if (Partie.gererNiveau.getNbDiamants() >= Partie.gererNiveau.getNiveau().getDiamonds_required()) {
-			Partie.gererNiveau
-					.setScore(Partie.gererNiveau.getScore() + Partie.gererNiveau.getNiveau().getDiamond_value_bonus());
-		} else {
-			Partie.gererNiveau
-					.setScore(Partie.gererNiveau.getScore() + Partie.gererNiveau.getNiveau().getDiamond_value());
-		}
-		Partie.gererNiveau.incrementerNbDiamants();
-	}
+    public void ramasserDiamant() {
+        if (Partie.gererNiveau.getNbDiamants() >= Partie.gererNiveau.getNiveau().getDiamonds_required()) {
+            Partie.gererNiveau
+                    .setScore(Partie.gererNiveau.getScore() + Partie.gererNiveau.getNiveau().getDiamond_value_bonus());
+        } else {
+            Partie.gererNiveau
+                    .setScore(Partie.gererNiveau.getScore() + Partie.gererNiveau.getNiveau().getDiamond_value());
+        }
+        Partie.gererNiveau.incrementerNbDiamants();
+    }
 
-	@Override
-	protected int contactAutreEntitee(Entitee entitee) {
-		if (entitee.is(Pierre)) {
-			if (entitee.getX() - getX() > 0
-					&& Partie.gererNiveau.getNiveau().getMap()[entitee.getX() + 1][entitee.getY()].is(Vide)) {
-				((entitees.tickables.Pierre) entitee).setDirection('d');
-				((entitees.tickables.Pierre) entitee).seDeplacer();
-			} else if (entitee.getX() - getX() < 0
-					&& Partie.gererNiveau.getNiveau().getMap()[entitee.getX() - 1][entitee.getY()].is(Vide)) {
-				((entitees.tickables.Pierre) entitee).setDirection('g');
-				((entitees.tickables.Pierre) entitee).seDeplacer();
-			}
-			return 0;
-		} else if (entitee.is(Diamant)) {
-			entitee.mourir();
-			ramasserDiamant();
-			return 1;
-		} else if (entitee.is(Sortie)) {
-			if (Partie.gererNiveau.getNbDiamants() >= Partie.gererNiveau.getNiveau().getDiamonds_required()) {
-				Partie.gererNiveau.setFiniSuccess(true);
-				Partie.gererNiveau.setDemandeFin(true);
-				return 1;
-			}
-			return 0;
-		} else if (entitee.is(Amibe)) {
-			mourir();
-			return -1;
-		} else if (entitee.is(Explosion)) {
-			mourir();
-			return -1;
-		}
-		return 1;
-	}
+    @Override
+    protected int contactAutreEntitee(Entitee entitee) {
+        if (entitee.is(Pierre)) {
+            if (entitee.getX() - getX() > 0
+                    && Partie.gererNiveau.getNiveau().getMap()[entitee.getX() + 1][entitee.getY()].is(Vide)) {
+                ((entitees.tickables.Pierre) entitee).setDirection('d');
+                ((entitees.tickables.Pierre) entitee).seDeplacer();
+            } else if (entitee.getX() - getX() < 0
+                    && Partie.gererNiveau.getNiveau().getMap()[entitee.getX() - 1][entitee.getY()].is(Vide)) {
+                ((entitees.tickables.Pierre) entitee).setDirection('g');
+                ((entitees.tickables.Pierre) entitee).seDeplacer();
+            }
+            return 0;
+        } else if (entitee.is(Diamant)) {
+            entitee.mourir();
+            ramasserDiamant();
+            return 1;
+        } else if (entitee.is(Sortie)) {
+            if (Partie.gererNiveau.getNbDiamants() >= Partie.gererNiveau.getNiveau().getDiamonds_required()) {
+                Partie.gererNiveau.setFiniSuccess(true);
+                Partie.gererNiveau.setDemandeFin(true);
+                return 1;
+            }
+            return 0;
+        } else if (entitee.is(Amibe)) {
+            mourir();
+            return -1;
+        } else if (entitee.is(Explosion)) {
+            mourir();
+            return -1;
+        } else if (entitee.is(Libellule)) {
+            mourir();
+            return -1;
+        } else if (entitee.is(Luciole)) {
+            mourir();
+            return -1;
+        }
+        return 1;
+    }
 
-	@Override
-	public void tick() {
-		deplacement();
-	}
+    @Override
+    public void tick() {
+        deplacement();
+    }
 
-	private void deplacement() {
-		setDirection(Partie.gererNiveau.getToucheClavier());
-		if (getDirection() != ' ') {
-			seDeplacer();
-		}
-	}
+    private void deplacement() {
+        setDirection(Partie.gererNiveau.getToucheClavier());
+        if (getDirection() != ' ') {
+            seDeplacer();
+        }
+    }
 
-	public boolean mourir() {
-		super.mourir();
-		Partie.gererNiveau.setDemandeReset(true);
-		return true;
-	}
+    public boolean mourir() {
+        super.mourir();
+        Partie.gererNiveau.setDemandeReset(true);
+        return true;
+    }
 
-	public char getAncienneDirection() {
-		return ancienneDirection;
-	}
+    public char getAncienneDirection() {
+        return ancienneDirection;
+    }
 
-	public void setAncienneDirection(char ancienneDirection) {
-		this.ancienneDirection = ancienneDirection;
-	}
+    public void setAncienneDirection(char ancienneDirection) {
+        this.ancienneDirection = ancienneDirection;
+    }
 
 }
