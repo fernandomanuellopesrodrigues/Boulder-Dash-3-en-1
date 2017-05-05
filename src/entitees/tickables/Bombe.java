82a26c6f616bc19bf24d982affb5c29870f29ab3
Dany BREGEON <dany@bregeon.net>
2017-05-05 14:54:46 +0200

    changements

---
+++ src/entitees/tickables/Bombe.java
@@ -0,0 +1,46 @@
+package entitees.tickables;
+
+import constantes.Constantes;
+import entitees.abstraites.Entitee;
+import entitees.abstraites.Tickable;
+import main.Partie;
+
+import static entitees.abstraites.Entitee.Entitees.Bombe;
+
+public class Bombe extends Tickable {
+	private int tempsRestantAvantExplosion = Constantes.BOOM;
+
+	protected Bombe(int x, int y) {
+		super(x, y);
+		enumeration = Bombe;
+		setDestructible(false);
+		Partie.gererNiveau.ajouterTickable(this);
+	}
+
+	@Override
+	protected int contactAutreEntitee(Entitee entitee) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void tick() {
+		if (tempsRestantAvantExplosion == 0 && !isMort()) {
+			exploser(false);
+		}
+		tempsRestantAvantExplosion--;
+	}
+
+	public boolean mourir() {
+		if (tempsRestantAvantExplosion > 0) {
+			tempsRestantAvantExplosion = -1;
+			exploser(false);
+			
+		}
+		return true;
+	}
+
+	public int getTempsRestantAvantExplosion() {
+		return tempsRestantAvantExplosion;
+	}
+}
